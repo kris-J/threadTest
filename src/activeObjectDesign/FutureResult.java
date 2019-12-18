a1563d9933f62a60ba205e711ee8b5d269702c67
fangjie <fangjie-0811@163.com>
2019-12-18 17:46:20 +0800

    主动对象模式

---
+++ src/activeObjectDesign/FutureResult.java
@@ -0,0 +1,31 @@
+package activeObjectDesign;
+
+/**
+ * @author fangjie
+ * @Description: ${todo}
+ * @date 2019/12/18 16:03
+ */
+public class FutureResult implements Result {
+
+    private Result result;
+
+    private boolean ready = false;
+
+    public synchronized void setResult(Result result) {
+        this.result = result;
+        ready = true;
+        this.notifyAll();
+    }
+
+    @Override
+    public synchronized Object getResultValue() {
+        while (!ready) {
+            try {
+                this.wait();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+        return result.getResultValue();
+    }
+}
---
+++ src/activeObjectDesign/Result.java
@@ -0,0 +1,11 @@
+package activeObjectDesign;
+
+/**
+ * @author fangjie
+ * @Description: ${todo}
+ * @date 2019/12/18 15:56
+ */
+public interface Result {
+
+    Object getResultValue();
+}
