d7047ac231d62dcb9f7281cf00be7963440ee9f2
fangjie <fangjie-0811@163.com>
2020-01-06 15:36:00 +0800

    cyclicBarrier

---
+++ src/main/java/cyclicBarrier/CyclicBarrierExample.java
@@ -0,0 +1,55 @@
+package cyclicBarrier;
+
+import java.util.concurrent.BrokenBarrierException;
+import java.util.concurrent.CyclicBarrier;
+
+/**
+ * @author fangjie
+ * @Description: ${todo}
+ * @date 2020/1/6 14:02
+ */
+public class CyclicBarrierExample {
+
+    public static void main(String[] args) throws BrokenBarrierException, InterruptedException {
+        CyclicBarrier cyclicBarrier = new CyclicBarrier(3, new Runnable() {
+            @Override
+            public void run() {
+                System.out.println("all cyclicBarrier finished.");
+            }
+        });
+        new Thread() {
+            @Override
+            public void run() {
+                try {
+                    Thread.sleep(5000);
+                    System.out.println(Thread.currentThread().getName() + " finished.");
+                    cyclicBarrier.await();
+                    System.out.println("cyclicBarrier wait finished");
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                } catch (BrokenBarrierException e) {
+                    e.printStackTrace();
+                }
+            }
+        }.start();
+
+        new Thread() {
+            @Override
+            public void run() {
+                try {
+                    Thread.sleep(10000);
+                    System.out.println(Thread.currentThread().getName() + " finished.");
+                    cyclicBarrier.await();
+                    System.out.println("cyclicBarrier wait finished");
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                } catch (BrokenBarrierException e) {
+                    e.printStackTrace();
+                }
+            }
+        }.start();
+
+        cyclicBarrier.await();
+        System.out.println("main wait finished");
+    }
+}
